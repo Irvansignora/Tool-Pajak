4a134a67d8d71458edff4ad744b3527ee20e90e6
Muhamad Irpan Yasin <143773773+Irvansignora@users.noreply.github.com>
2026-05-21 11:00:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Setiap kali ada versi baru, user diberi notifikasi.
 // ============================================================
 
-const CACHE_NAME = 'pajak-tools-v13';
+const CACHE_NAME = 'pajak-tools-v14';
 
 // Semua file yang di-cache saat install (precache)
 const PRECACHE_URLS = [
